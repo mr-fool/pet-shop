a115da49fadbd344a120ac4aef7c2ff175eaa1e4
mr-fool <mr-fool@users.noreply.github.com>
2019-03-06 10:52:40 -0700

    installed dotenv

--- truffle-config.js
+++ truffle-config.js
@@ -1,11 +1,20 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+require('dotenv').config()  // Store environment-specific variable from '.env' to process.env
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // for more about customizing your Truffle configuration!
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
+      host: 'localhost',
+      port: 8545,
+      network_id: '*' // Match any network id
+    },
+    
+    rinkeby: {
+      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY),
+      network_id: 4,
+      gas: 3000000,
+      gasPrice: 10000000000
     }
   }
-};
+}
